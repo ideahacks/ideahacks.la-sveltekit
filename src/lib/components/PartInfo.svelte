80d8bb9dfa10be63f3887e5e54dda56a53981c20
KevZ1209 <kevinbowditch11@gmail.com>
2023-06-07 19:32:05 -0700

    add part info component

---
+++ src/lib/components/PartInfo.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  interface Info {
+    partName: string;
+    partDescription: string;
+    imgPath: string;
+    id: string;
+  }
+
+  export let info: Info;
+
+</script>
+  
+  <div class="card card-compact w-64 bg-base-100 shadow-xl">
+    <figure><img src={info.imgPath} alt={info.partName} /></figure>
+    <div class="card-body">
+      <h2 class="card-title">{info.partName}</h2>
+      <p>Description: {info.partDescription}</p>
+      <div class="card-actions justify-end">
+      </div>
+    </div>
+  </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,35 @@
+<script>
+  import PartInfo from "../lib/components/PartInfo.svelte"
+
+  let sample_part_info = {
+    "partName": "Part 1",
+    "partDescription": "Here's a part that is used as a sample.",
+    "imgPath": "src/lib/images/sample-img.jpg",
+    "id": "1"
+  }
+
+  let sample_part_info2 = {
+    "partName": "Part 2",
+    "partDescription": "Lorem Ipsum Lorem Ipsum",
+    "imgPath": "src/lib/images/sample-img.jpg",
+    "id": "2"
+  }
+
+</script>
+
 <h1 class="text-3xl font-bold underline">
   Hello world!
 </h1>
 
+<PartInfo info={sample_part_info}/>
+<PartInfo info={sample_part_info2}/>
+
 <style lang="postcss">
+  
+  
   :global(html) {
     background-color: theme(colors.gray.100);
   }
+
+
 </style>
