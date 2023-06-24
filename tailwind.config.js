d50b3602d067e938883ca00b0732f8fda57d9067
Paul Zhang <paulz@ucla.edu>
2023-06-24 16:38:22 -0500

    Add custom fonts: Cehua, Glacial Indifference, Open Sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		fontFamily: {
+			'display-serif': ['Cehua', ...defaultTheme.fontFamily.serif],
+			'display-sans': ['Glacial Indifference', ...defaultTheme.fontFamily.sans]
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Open Sans', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 	plugins: [require('daisyui')],
 
