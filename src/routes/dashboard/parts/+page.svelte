b69ddf946323b08dc89e85b7105374e13fa755de
Paul Zhang <paulz@ucla.edu>
2023-06-24 14:34:56 -0500

    Add Fuse.js for fuzzy text search of parts db

---
+++ src/routes/dashboard/parts/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Fuse from 'fuse.js';
+
+	const parts = [
+		{
+			title: 'lol'
+		},
+		{
+			title: 'lmao'
+		},
+		{
+			title: 'apple'
+		}
+	];
+
+	const options = {
+		keys: ['title']
+	};
+
+	const fuse = new Fuse(parts, options);
+
+	$: filteredParts = fuse.search('apple');
+</script>
+
+<div class="text-xl">{JSON.stringify(filteredParts)}</div>
