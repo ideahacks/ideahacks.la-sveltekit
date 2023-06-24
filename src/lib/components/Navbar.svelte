8c7c4169d09a31515794e42147869e2bc99b7b90
KevZ1209 <kevinbowditch11@gmail.com>
2023-06-24 13:14:02 -0700

    initial navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,27 @@
+<div class="navbar bg-red-950 text-white">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <label tabindex="0" class="btn btn-ghost md:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </label>
+        <ul tabindex="0" class="bg-red-950 menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <li class="hover:text-yellow-500"><a>ABOUT</a></li>
+          <li class="hover:text-yellow-500"><a>SCHEDULE</a></li>
+          <li class="hover:text-yellow-500"><a>FAQ</a></li>
+          <li class="hover:text-yellow-500"><a>SPONSORS</a></li>
+        </ul>
+      </div>
+      <p class="text-xl ">IDEA HACKS</p>
+    </div>
+    <div class="navbar-center hidden md:flex">
+      <ul class="menu menu-horizontal px-1 ">
+        <li class="hover:text-yellow-500"><a>ABOUT</a></li>
+        <li class="hover:text-yellow-500"><a>SCHEDULE</a></li>
+        <li class="hover:text-yellow-500"><a>FAQ</a></li>
+        <li class="hover:text-yellow-500"><a>SPONSORS</a></li>
+      </ul>
+    </div>
+    <div class="navbar-end">
+      <a class="text-yellow-500 btn btn-ghost hover:bg-black p-2">LOGIN</a>
+    </div>
+  </div>
