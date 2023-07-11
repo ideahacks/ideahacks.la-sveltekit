29333e8ee9b039650786eb1e9644e481d413c249
KevZ1209 <kevinbowditch11@gmail.com>
2023-07-11 10:47:13 -0700

    add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script>
   import "../app.css";
   import Navbar from "../lib/components/Navbar.svelte";
+  import Footer from "../lib/components/Footer.svelte";
 </script>
 
 <Navbar/>
 
+
+
 <slot />
+
+<Footer/>
