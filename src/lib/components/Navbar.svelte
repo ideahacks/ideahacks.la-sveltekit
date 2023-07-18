d2f4812c1e3a5d6ee62b5c3833fd5452c0b06a8f
Paul Zhang <paulz@ucla.edu>
2023-07-17 22:27:44 -0500

    Navbar: Use details and summary tags

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,43 +1,56 @@
 <div class="navbar bg-red-950 text-white">
-  <div class="navbar-start">
-    
-    <!-- HAMBURGER MENU -->
-    <div class="dropdown">
-      <label tabindex="0" class="btn btn-ghost md:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabindex="0" class="bg-red-950 menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li class="py-0"><a>ABOUT</a></li>
-        <ul tabindex="0" class="ml-4 bg-red-950 ">
-          <li class="hover:text-yellow-500 text-xs"><a>VENUE</a></li>
-          <li class="hover:text-yellow-500 text-xs"><a>TEAM</a></li>
-          <li class="hover:text-yellow-500 text-xs"><a>HISTORY</a></li>
-        </ul>
-        <li class="hover:text-yellow-500"><a>SCHEDULE</a></li>
-        <li class="hover:text-yellow-500"><a>FAQ</a></li>
-        <li class="hover:text-yellow-500"><a>SPONSORS</a></li>
-      </ul>
-    </div>
-    <img class="w-12" src="src/lib/images/ideahacks_logo.png" alt="ideahacks"/>
-  </div>
-  
-  <!-- NAVBAR -->
-  <div class="navbar-center hidden md:flex">
-    <ul class="menu menu-horizontal px-1 ">
-      <div class="dropdown dropdown-bottom">
-        <label tabindex="0" class="btn btn-ghost  m-1 bg-red-950 ">ABOUT &#x25BC</label>
-        <ul tabindex="0" class="bg-red-950 menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box">
-          <li class="hover:text-yellow-500 text-xs"><a>VENUE</a></li>
-          <li class="hover:text-yellow-500 text-xs"><a>TEAM</a></li>
-          <li class="hover:text-yellow-500 text-xs"><a>HISTORY</a></li>
-        </ul>
-      </div>
-      <li class="hover:text-yellow-500"><a>SCHEDULE</a></li>
-      <li class="hover:text-yellow-500"><a>FAQ</a></li>
-      <li class="hover:text-yellow-500"><a>SPONSORS</a></li>
-    </ul>
-  </div>
-  <div class="navbar-end">
-    <a class="text-yellow-500 btn btn-ghost hover:bg-black p-2">LOGIN</a>
-  </div>
+	<div class="navbar-start">
+		<!-- HAMBURGER MENU -->
+		<details class="dropdown">
+			<summary class="btn-ghost btn md:hidden">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h8m-8 6h16"
+					/></svg
+				>
+			</summary>
+			<ul
+				class="dropdown-content menu rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 bg-red-950 p-2 shadow"
+			>
+				<li class="py-0"><a>ABOUT</a></li>
+				<ul class="ml-4 bg-red-950">
+					<li class="text-xs hover:text-yellow-500"><a>VENUE</a></li>
+					<li class="text-xs hover:text-yellow-500"><a>TEAM</a></li>
+					<li class="text-xs hover:text-yellow-500"><a>HISTORY</a></li>
+				</ul>
+				<li class="hover:text-yellow-500"><a>SCHEDULE</a></li>
+				<li class="hover:text-yellow-500"><a>FAQ</a></li>
+				<li class="hover:text-yellow-500"><a>SPONSORS</a></li>
+			</ul>
+		</details>
+		<img class="w-12" src="src/lib/images/ideahacks_logo.png" alt="ideahacks" />
+	</div>
+
+	<!-- NAVBAR -->
+	<div class="navbar-center hidden md:flex">
+		<ul class="menu menu-horizontal px-1">
+			<details class="dropdown-bottom dropdown">
+				<summary class="btn-ghost btn m-1 bg-red-950">ABOUT &#x25BC</summary>
+				<ul class="dropdown-content menu rounded-box menu-sm z-[1] mt-3 bg-red-950 p-2 shadow">
+					<li class="text-xs hover:text-yellow-500"><a>VENUE</a></li>
+					<li class="text-xs hover:text-yellow-500"><a>TEAM</a></li>
+					<li class="text-xs hover:text-yellow-500"><a>HISTORY</a></li>
+				</ul>
+			</details>
+			<li class="hover:text-yellow-500"><a>SCHEDULE</a></li>
+			<li class="hover:text-yellow-500"><a>FAQ</a></li>
+			<li class="hover:text-yellow-500"><a>SPONSORS</a></li>
+		</ul>
+	</div>
+	<div class="navbar-end">
+		<a class="btn-ghost btn p-2 text-yellow-500 hover:bg-black">LOGIN</a>
+	</div>
 </div>
