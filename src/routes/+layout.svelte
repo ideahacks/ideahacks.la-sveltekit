23393e0c6292d7c88a2d2a8c72eb8b71202d9a96
Paul Zhang <paulz@ucla.edu>
2023-07-29 19:03:21 -0500

    Format project using `npm run format`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,11 @@
 <script>
-  import "../app.css";
-  import Navbar from "../lib/components/Navbar.svelte";
-  import Footer from "../lib/components/Footer.svelte";
+	import '../app.css';
+	import Navbar from '../lib/components/Navbar.svelte';
+	import Footer from '../lib/components/Footer.svelte';
 </script>
 
-<Navbar/>
-
-
+<Navbar />
 
 <slot />
 
-<Footer/>
+<Footer />
