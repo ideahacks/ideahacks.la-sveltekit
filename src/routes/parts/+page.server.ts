a2b6816ad81e29bde8f46aa9b5ca1b2eb9fabee4
Shannon <38598412+swang1111@users.noreply.github.com>
2023-09-18 17:12:01 -0700

    add fuzzy search

--- src/routes/parts/+page.server.ts
+++ src/routes/parts/+page.server.ts
@@ -1,11 +1,39 @@
-import prisma from "$lib/prisma";
+import prisma from '$lib/prisma';
+import Fuse from 'fuse.js';
 
 export async function load() {
     const parts = await prisma.part.findMany();
+	
+	const fuseOptions = {
+		// isCaseSensitive: false,
+		// includeScore: false,
+		// shouldSort: true,
+		// includeMatches: false,
+		// findAllMatches: false,
+		// minMatchCharLength: 1,
+		// location: 0,
+		// threshold: 0.6,
+		// distance: 100,
+		// useExtendedSearch: false,
+		// ignoreLocation: false,
+		// ignoreFieldNorm: false,
+		// fieldNormWeight: 1,
+		keys: [
+			"name"
+		]
+	};
+	
+	const fuse = new Fuse(parts, fuseOptions);
+	
+	// Change the pattern
+	const searchPattern = "con"
+	
+	const fuseResults = fuse.search(searchPattern)
+
 	return {
-		summaries: parts.map((parts) => ({
-			name: parts.name,
-			quantity: parts.quantity
+		summaries: fuseResults.map((parts) => ({
+			name: parts.item.name,
+			quantity: parts.item.quantity
 		}))
 	};
 }
