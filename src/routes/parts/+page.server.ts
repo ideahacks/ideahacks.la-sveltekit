bcab0531f4714656cd681f88027b61da63296b72
Shannon <38598412+swang1111@users.noreply.github.com>
2023-10-26 18:51:00 -0700

    added search bar w/ fuzzy search

--- src/routes/parts/+page.server.ts
+++ src/routes/parts/+page.server.ts
@@ -1,39 +1,6 @@
 import prisma from '$lib/prisma';
-import Fuse from 'fuse.js';
 
 export async function load() {
-    const parts = await prisma.part.findMany();
-	
-	const fuseOptions = {
-		// isCaseSensitive: false,
-		// includeScore: false,
-		// shouldSort: true,
-		// includeMatches: false,
-		// findAllMatches: false,
-		// minMatchCharLength: 1,
-		// location: 0,
-		// threshold: 0.6,
-		// distance: 100,
-		// useExtendedSearch: false,
-		// ignoreLocation: false,
-		// ignoreFieldNorm: false,
-		// fieldNormWeight: 1,
-		keys: [
-			"name"
-		]
-	};
-	
-	const fuse = new Fuse(parts, fuseOptions);
-	
-	// Change the pattern
-	const searchPattern = "con"
-	
-	const fuseResults = fuse.search(searchPattern)
-
-	return {
-		summaries: fuseResults.map((parts) => ({
-			name: parts.item.name,
-			quantity: parts.item.quantity
-		}))
-	};
+	const parts = await prisma.part.findMany();
+	return { parts };
 }
--- src/routes/parts/+page.svelte
+++ src/routes/parts/+page.svelte
@@ -1,14 +1,49 @@
 <script>
-    export let data;
+	export let data;
+
+	import Fuse from 'fuse.js';
+
+	const parts = data.parts;
+
+	const fuseOptions = {
+		// isCaseSensitive: false,
+		// includeScore: false,
+		// shouldSort: true,
+		// includeMatches: false,
+		// findAllMatches: false,
+		// minMatchCharLength: 1,
+		// location: 0,
+		// threshold: 0.6,
+		// distance: 100,
+		// useExtendedSearch: false,
+		// ignoreLocation: false,
+		// ignoreFieldNorm: false,
+		// fieldNormWeight: 1,
+		keys: ['name']
+	};
+
+	const fuse = new Fuse(parts, fuseOptions);
+
+	// Change the pattern
+	let searchPattern = '';
+
+	$: fuseResults = fuse.search(searchPattern);
+
+	$: summaries = fuseResults.map((parts) => ({
+		name: parts.item.name,
+		quantity: parts.item.quantity
+	}));
 </script>
 
 <h1>Parts</h1>
 <p>List of available parts.</p>
 
-<br>
+<br />
+
+<input class="input-bordered input" bind:value={searchPattern} />
 
 <ul>
-	{#each data.summaries as { name, quantity }}
+	{#each summaries as { name, quantity }}
 		<li>{name}: {quantity}</li>
 	{/each}
 </ul>
