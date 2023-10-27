145aab228c28eb55a47d5c1973f9371ef5a913ea
Brandon Tran <tranbrandon1233@gmail.com>
2023-10-26 18:12:53 -0700

    Fix to OAuth

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,12 +7,27 @@ import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY)
 
 async function signUp(){
-	await supabase.auth.signInWithOAuth({
+	const {error} = await supabase.auth.signInWithOAuth({
 			provider: 'google',
 			options: {
 				redirectTo: 'http://localhost:5173/profile'
 			}
 		});
+		const user = supabase.auth.getSession()['data'].session.user
+		if (error) {
+			console.error('Error during sign in:', error)
+		} else if (user && user.email.endsWith('@g.ucla.edu')) {
+			console.log('User signed in:', user)
+			await supabase.auth.signInWithOAuth({
+				provider: 'google',
+				options: {
+					redirectTo: 'http://localhost:5173/profile'
+				}
+		});
+		} else {
+			console.log('Invalid domain. Please use a g.ucla.edu email.')
+		}
+}
 	/*
 	let uname = (document.getElementById('uname') as HTMLInputElement).value
 	let pass = (document.getElementById('pass') as HTMLInputElement).value
@@ -26,7 +41,7 @@ async function signUp(){
 		});
 	}
 */
-}
+
 </script>
 
 <div class="grow w-full flex justify-center items-center bg-neutral-100">
@@ -40,7 +55,7 @@ async function signUp(){
 		
 		<button
 			class="bg-black text-white w-full text-center py-4 px-4 mt-2 hover:shadow-lg"
-			on:click={signUp}>Sign Up</button
+			on:click={signUp}>Sign Up with Google OAuth</button
 		>
 	</div>
 </div>
