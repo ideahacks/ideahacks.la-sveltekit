1df8c094c167ef9127d1e2b47aae3f18cb5e5a4d
Paul Zhang <paulz@ucla.edu>
2023-11-10 23:30:48 -0800

    Remove old, unused auth code

--- src/routes/auth/testing/+server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export const GET = async ({ locals: { supabase } }) => {
-	// logout of supabase
-	await supabase.auth.signOut();
-
-	throw redirect(301, '/');
-};
