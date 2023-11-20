e6709cab05359bf5b4f82b4d18e64c221dc2a255
Paul Zhang <paulz@ucla.edu>
2023-11-20 03:34:12 -0800

    Add description and FAQ to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { MailCheck, Send, Sparkles } from 'lucide-svelte';
+	import { MailCheck, Send, Sparkles, Sun } from 'lucide-svelte';
 	import type { ActionData } from './$types.js';
 	import { enhance } from '$app/forms';
 
@@ -77,6 +77,78 @@
 	<h2 class="font-display-sans text-6xl font-black">Jan 12-14</h2>
 	<div class="divider" />
 	<a href="/apply" class="btn btn-primary btn-lg my-8">Applications are open! <Sparkles /></a>
+
+	<p class="mb-4">
+		IEEE at UCLA is pleased to present the tenth anniversary of IDEA Hacks, the premier
+		hardware-focused hackathon on the West Coast. Hosted by IEEE’s Student Branch at UCLA for 36
+		hours this upcoming January 12-14, 2024, it provides hundreds of students from both UCLA and
+		local community colleges with the opportunity to develop their own tangible products. Throughout
+		the experience, IEEE provides parts, tools, space, food, and workshops all for free, so all
+		students can focus solely on their projects.
+	</p>
+	<p class="mb-4">
+		This year will be the tenth anniversary of IDEA Hacks, and the theme “Celebrating Our
+		Communities” will encourage students from all backgrounds to embrace their unique backgrounds
+		and create something that will improve or benefit their communities, surroundings, and
+		environments. The three sub-categories - sustainability, accessibility, and “go touch grass”
+		(going outside) - are aimed to promote the creation of projects that will benefit the health of
+		the planet, improve usability, or share the enjoyment of life. Everything you need throughout
+		the weekend, from parts to food to knowledge, will all be right at your fingertips.
+		Communication and announcements will happen through a Discord server we will set up for the
+		hackathon.
+	</p>
+
+	<h2 class="mt-4 font-display-serif text-5xl">Frequently asked questions</h2>
+
+	<div class="grid gap-2 md:grid-cols-2">
+		<div class="card">
+			<div class="card-body">
+				<h3 class="card-title font-display-sans text-3xl">Who can attend?</h3>
+				<p>
+					All students interested are encouraged to apply regardless of your major or experience
+					level! And although most participants end up being UCLA engineering students, we would
+					love to welcome participants from any major and those outside the Bruin community :)
+				</p>
+			</div>
+		</div>
+		<div class="card">
+			<div class="card-body">
+				<h3 class="card-title font-display-sans text-3xl">What if I don't have a team?</h3>
+				<p>
+					You are free to hack alone, but doing it with a team makes it all the more fun! We
+					encourage everyone to be on teams of around 4-5 hackers. Not only will this make it more
+					enjoyable, but more equipment will be available for your use.
+				</p>
+			</div>
+		</div>
+		<div class="card">
+			<div class="card-body">
+				<h3 class="card-title font-display-sans text-3xl">
+					What tools and hardware will be provided for my team?
+				</h3>
+				<p>
+					If you would like to see a specific part or device at the hackathon, you can specify so on
+					your application and we'll do our best to accommodate! You can also view a full list of
+					available parts on our Parts page closer to the start of the hackathon. We will also
+					provide soldering stations, 3D printers, and laser cutters for you to use on your
+					projects.
+				</p>
+			</div>
+		</div>
+		<div class="card">
+			<div class="card-body">
+				<h3 class="card-title font-display-sans text-3xl">How do I contact IDEA Hacks?</h3>
+				<p>
+					Email us at <a href="mailto:evp@ieeebruins.com" class="link">evp@ieeebruins.com</a>! If
+					you have feedback on the website please reach out at
+					<a href="mailto:webmaster@ieeebruins.com" class="link">webmaster@ieeebruins.com</a>.
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<h2 class="my-4 mt-4 font-display-serif text-5xl">Sponsoring</h2>
+
 	<p>
 		Interested in sponsoring IDEA Hacks? Check out our <a
 			href="https://drive.google.com/file/d/1KDASmkWqPmwoqTGyly87Hjs6MYoJ3MSI/view?usp=sharing"
