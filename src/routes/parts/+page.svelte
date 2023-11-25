258b97879eff03bc4b1cef2fb860fbfe86aba3e4
Shannon <38598412+swang1111@users.noreply.github.com>
2023-11-25 13:51:08 -0800

    Display all parts when search is empty, improve UI

--- src/routes/parts/+page.svelte
+++ src/routes/parts/+page.svelte
@@ -24,10 +24,16 @@
 
 	const fuse = new Fuse(parts, fuseOptions);
 
+	const restructuredParts = parts.map(parts => ({
+		item: parts,
+		matches: [],
+		score: 1
+	}));
+
 	// Change the pattern
 	let searchPattern = '';
 
-	$: fuseResults = fuse.search(searchPattern);
+	$: fuseResults = searchPattern == '' ? restructuredParts : fuse.search(searchPattern);
 
 	$: summaries = fuseResults.map((parts) => ({
 		name: parts.item.name,
@@ -35,25 +41,33 @@
 	}));
 </script>
 
-<h1>Parts</h1>
-<p>List of available parts.</p>
+<div class="m-12 space-y-8 justify-center">
+	<h1 class="flex justify-center font-display-serif text-8xl md:text-9xl">Parts</h1>
+	<!-- <p>List of available parts.</p> -->
 
-<br />
+	<br />
+	<div class="flex justify-center pb-4">
+		<input class="input-bordered input" placeholder="Search" bind:value={searchPattern} />
+	</div>
 
-<input class="input-bordered input" bind:value={searchPattern} />
 
-<div class="flex">
-	{#each summaries as { name, quantity }}
-		<!-- <li>{name}: {quantity}</li> -->
-		<div class="card w-96 bg-base-100 shadow-xl">
-			<!-- <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure> -->
-			<div class="card-body">
-				<h2 class="card-title">{name}</h2>
-				<p>{quantity}</p>
-				<div class="card-actions justify-end">
-					<button class="btn w-64 rounded-full">Checkout</button>
+	<div class="flex justify-center flex-wrap gap-8">
+		{#each summaries as { name, quantity }}
+			<!-- <li>{name}: {quantity}</li> -->
+			<div class="card w-96 bg-base-100 shadow-xl">
+				<figure class="px-10 pt-10">
+					<img src="https://hackster.imgix.net/uploads/attachments/1443631/_ArRPjgFw5x.blob?auto=compress%2Cformat&w=900&h=675&fit=min" alt="LED" />
+				</figure>
+				<div class="card-body gap-4">
+					<h2 class="card-title">{name}</h2>
+					<p>Quantity: {quantity}</p>
+					<p>Description: ...</p>
+					<div class="card-actions justify-center pt-2">
+						<button class="btn w-64 rounded-full">Checkout</button>
+					</div>
 				</div>
 			</div>
-		</div>
-	{/each}
+		{/each}
+	</div>
+
 </div>
