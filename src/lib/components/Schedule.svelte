7e78df64692a9e33d87e881366772770bcffa3d2
Paul Zhang <paulz@ucla.edu>
2023-11-29 17:55:23 -0800

    Add preliminary schedule for 2024 hackathon

--- src/lib/components/Schedule.svelte
+++ src/lib/components/Schedule.svelte
@@ -1,285 +1,109 @@
-<body>
-	<div id="whole">
-		<title>Schedule</title>
+<script>
+	import { Sparkles } from 'lucide-svelte';
+</script>
 
-		<h1 class="text-3xl font-bold underline">Schedule</h1>
+<h2 class="pt-4 font-display-sans text-3xl font-bold">Friday, January 20th</h2>
+<div class="overflow-x-auto">
+	<table class="table-lg table">
+		<!-- head -->
+		<thead>
+			<tr>
+				<th>Event</th>
+				<th>Time / Location</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th>Webdev Workshop</th>
+				<td>4:00 pm - 4:45 pm <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+			<tr>
+				<th>Check In</th>
+				<td>4:30 pm - 8:00 pm <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+			<tr>
+				<th>Team Building Workshop</th>
+				<td>7:00 pm - 8:00 pm <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+			<tr>
+				<th>Opening Presentation</th>
+				<td>8:30 pm - 9:00 pm <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+			<tr>
+				<th class="flex gap-1">Hacking Begins <Sparkles /></th>
+				<td>9:00 pm <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+			<tr>
+				<th>Microcontroller Workshop</th>
+				<td>9:15 pm - 9:45 pm <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+			<tr>
+				<th>Wireless Connectivity Workshop</th>
+				<td>10:00 pm - 10:30 pm <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
 
-		<br />
-		<br />
+<br />
+<br />
 
-		<h2 style="text-indent: 20px;">FRIDAY, JANUARY 20</h2>
-		<div class="overflow-x-auto">
-			<table class="table">
-				<!-- head -->
-				<thead>
-					<tr />
-				</thead>
-				<tbody>
-					<!-- row 1 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-					<!-- row 2 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-					<!-- row 3 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
+<h2 class="pt-4 font-display-sans text-3xl font-bold">Saturday, January 21st</h2>
+<div class="overflow-x-auto">
+	<table class="table-lg table">
+		<thead>
+			<tr>
+				<th>Event</th>
+				<th>Time</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th>Breakfast</th>
+				<td>8:00 am - 10:00 am <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+			<tr>
+				<th>Lunch</th>
+				<td>12:00 pm - 2:00 pm <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+			<tr>
+				<th>Dinner</th>
+				<td>6:00 pm - 8:00 pm <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
 
-					<!-- row 4 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 5 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 6 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 7 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 8 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 9 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 10 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 11 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 12 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-
-		<br />
-		<br />
-
-		<h2 style="text-indent: 20px;">SATURDAY, JANUARY 21</h2>
-		<div class="overflow-x-auto">
-			<table class="table">
-				<!-- head -->
-				<thead>
-					<tr />
-				</thead>
-				<tbody>
-					<!-- row 1 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-					<!-- row 2 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-					<!-- row 3 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 4 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 5 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 6 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 7 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 8 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 9 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 10 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 11 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 12 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-
-		<br />
-		<br />
-
-		<h2 style="text-indent: 20px;">SUNDAY, JANUARY 22</h2>
-		<div class="overflow-x-auto">
-			<table class="table">
-				<!-- head -->
-				<thead>
-					<tr />
-				</thead>
-				<tbody>
-					<!-- row 1 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-					<!-- row 2 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-					<!-- row 3 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 4 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 5 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 6 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 7 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 8 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 9 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 10 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 11 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-
-					<!-- row 12 -->
-					<tr>
-						<td id="col1">4PM - 8PM: CHECK-IN</td>
-						<td>Ackerman Grand Ballroom</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
-</body>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-
-	h1 {
-		text-align: center;
-	}
-	h2 {
-		text-align: left;
-		font-size: x-large;
-	}
-	body {
-		background-color: rgb(146, 27, 27);
-	}
-	#whole {
-		margin: 80px;
-		background-color: rgb(78, 7, 7);
-		color: white;
-	}
-	#col1 {
-		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-		font-weight: bolder;
-	}
-</style>
+<h2 class="pt-4 font-display-sans text-3xl font-bold">Sunday, January 22nd</h2>
+<div class="overflow-x-auto">
+	<table class="table-lg table">
+		<thead>
+			<tr>
+				<th>Event</th>
+				<th>Time</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th>Breakfast</th>
+				<td>7:00 am - 9:00 am <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+			<tr>
+				<th class="flex gap-1">Hacking Ends <Sparkles /></th>
+				<td>9:00 am <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+			<tr>
+				<th>Round 1 Judging</th>
+				<td>9:15 am - 10:45 am <span class="badge">TBD</span></td>
+			</tr>
+			<tr>
+				<th>Judging Finals</th>
+				<td>11:00 am - 12:30 pm <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+			<tr>
+				<th>Awards and Closing</th>
+				<td>12:40 pm - 1:00 pm <span class="badge">Ackerman Grand Ballroom</span></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
