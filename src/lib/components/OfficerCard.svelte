5bb8faaf92fec40ba7791d12e1bd2421d9064f09
KevZ1209 <kevinbowditch11@gmail.com>
2023-12-31 21:33:17 -0500

    add hover for cat pic

--- src/lib/components/OfficerCard.svelte
+++ src/lib/components/OfficerCard.svelte
@@ -1,15 +1,30 @@
 <script>
 	// @ts-nocheck
 	export let officerInfo;
+
+	let catMode = false;
 </script>
 
-<div class="card w-56 bg-gray-100 bg-opacity-10 shadow-xl">
+<div
+	role="tooltip"
+	class="card w-56 bg-gray-100 bg-opacity-10 shadow-xl"
+	on:mouseenter={() => (catMode = true)}
+	on:mouseleave={() => (catMode = false)}
+>
 	<figure class="px-5 pt-5">
-		<img
-			src={officerInfo.cat_img_src}
-			alt={officerInfo.name + ' as a cat'}
-			class="h-48 w-48 rounded-xl object-cover"
-		/>
+		{#if catMode}
+			<img
+				src={officerInfo.cat_img_src}
+				alt={officerInfo.name + ' as a cat'}
+				class="h-48 w-48 rounded-xl object-cover"
+			/>
+		{:else}
+			<img
+				src={officerInfo.img_src}
+				alt={officerInfo.name}
+				class="h-48 w-48 rounded-xl object-cover"
+			/>
+		{/if}
 	</figure>
 	<div class="card-body items-center text-center">
 		<h2 class="font-bold">{officerInfo.name}</h2>
