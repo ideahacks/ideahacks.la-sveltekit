b836344f753524d917b2df633500701c55f0387e
Paul Zhang <paulz@ucla.edu>
2024-01-12 14:34:21 -0800

    Display teams and owned parts for admins (#34)

---
+++ src/routes/admin/teams/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+	import { TerminalSquare } from 'lucide-svelte';
+	import { stringify } from 'postcss';
+
+	export let data;
+
+	$: checkouts = data.teams.reduce((checkouts, team) => {
+		const teamOwnedParts = data.teamsParts
+			.filter((teamPart) => teamPart.team_id === team.team_id)
+			.map((teamPart) => {
+				const partMetadata = data.parts.find((part) => part.part_id === teamPart.part_id);
+				return {
+					part_id: teamPart.part_id,
+					name: partMetadata?.name,
+					quantity: teamPart.quantity,
+					total_quantity: partMetadata?.quantity
+				};
+			});
+
+		checkouts[team.team_id] = teamOwnedParts;
+
+		return checkouts;
+	}, {});
+</script>
+
+<div class="m-12 justify-center space-y-8">
+	<h1 class="text-center font-display-serif text-3xl md:text-4xl">Teams and Parts</h1>
+	<div class="overflow-x-auto">
+		<table class="table table-lg">
+			<thead>
+				<tr>
+					<th>Team Number</th>
+					<th>Parts (ID, Name, Quantity/Total)</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each data.teams as teams}
+					<tr>
+						<th>{teams.team_id}</th>
+						<td
+							>{#each checkouts[teams.team_id] as part}
+								<p>{JSON.stringify(part)}</p>
+							{/each}</td
+						>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+</div>
