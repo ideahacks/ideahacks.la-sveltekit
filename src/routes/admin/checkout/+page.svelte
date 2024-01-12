9741f090d04948f35bf9bada1d22a994addb7c16
Paul Zhang <paulz@ucla.edu>
2024-01-12 11:14:01 -0800

    Implement checking out parts

--- src/routes/admin/checkout/+page.svelte
+++ src/routes/admin/checkout/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { redirect } from '@sveltejs/kit';
 	import fuzzysort from 'fuzzysort';
+	import { ScanBarcode } from 'lucide-svelte';
 
 	export let data;
 
@@ -19,58 +23,132 @@
 		threshold: -100
 	});
 
-	let cart = new Set();
+	$: serverResponse = null;
+
+	let cart: { id: number; quantity: number }[] = [];
+	let teamNumber: number;
+
+	function addToCart(id: number) {
+		if (cart.find((part) => part.id === id)) {
+			return;
+		}
+
+		cart = [...cart, { id, quantity: 1 }];
+	}
+
+	function removeFromCart(id: number) {
+		cart = cart.filter((part) => part.id !== id);
+	}
+
+	async function checkout() {
+		const response = await fetch($page.url.pathname, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ teamNumber, cart })
+		});
+
+		if (!response.ok) {
+			return;
+		}
+
+		serverResponse = await response.json();
+	}
 </script>
 
-<div class=" m-12 justify-center space-y-8">
-	<h1 class="text-center font-display-serif text-6xl md:text-8xl">Parts Checkout</h1>
-	<div class="flex">
-		<div class="w-1/2">
-			<h2 class="text-center font-display-serif text-3xl">Search</h2>
-			<div class="flex justify-center gap-2 pb-4">
-				<input
-					class="input input-bordered"
-					placeholder="Search for a part by ID"
-					bind:value={search}
-				/>
+{#if serverResponse}
+	<div class="m-12 flex flex-col justify-center gap-2 space-y-8">
+		<h1 class="text-center font-display-serif text-3xl md:text-4xl">Parts Checkout</h1>
+		<p class="text-center font-bold">Successful checkout for team {serverResponse.teamNumber}!</p>
+		<p class="text-center">{JSON.stringify(serverResponse.partsData)}</p>
+		<button
+			class="btn btn-primary"
+			on:click={() => {
+				serverResponse = null;
+				cart = [];
+				teamNumber = undefined;
+			}}>Return to Checkout Page</button
+		>
+	</div>
+{:else}
+	<div class="m-12 justify-center space-y-8">
+		<h1 class="text-center font-display-serif text-3xl md:text-4xl">Parts Checkout</h1>
+		<div class="flex">
+			<div class="m-4 w-1/2">
+				<h2 class="mb-4 text-center font-display-serif text-3xl">Search</h2>
+				<div class="flex justify-center gap-2 pb-4">
+					<div class="btn btn-primary"><ScanBarcode /></div>
+					<form>
+						<input
+							class="input input-bordered"
+							placeholder="Search for a part by ID"
+							bind:value={search}
+						/>
+					</form>
+				</div>
+
+				<p class="my-4 text-center font-display-sans text-xl">
+					You're currently viewing {results.length}
+					{results.length === 1 ? 'part' : 'parts'}
+				</p>
+
+				<div class="flex flex-wrap justify-center gap-4">
+					{#each results as { score, obj: { id, name, quantity, image_url, all_ids } }}
+						<div class="card w-60 border border-opacity-50">
+							<div class="card-body">
+								<div class="card-actions">
+									<button
+										class="btn btn-primary"
+										on:click={() => {
+											search = '';
+											addToCart(id);
+										}}>Add to cart</button
+									>
+								</div>
+								<p>IDs: {all_ids}</p>
+								<p>Search score: {score}</p>
+								<h2 class="card-title">{name}</h2>
+								<div class="badge">Quantity: {quantity}</div>
+							</div>
+							<figure>
+								<img src={image_url} alt={name} />
+							</figure>
+						</div>
+					{/each}
+				</div>
 			</div>
-
-			<p class="text-center">Items in cart: [{[...cart].join(', ')}]</p>
-
-			<p class="text-center font-display-sans text-xl">
-				You're currently viewing {results.length}
-				{results.length === 1 ? 'part' : 'parts'}
-			</p>
-
-			<div class="flex flex-wrap justify-center gap-4">
-				<!-- Exact match returns a score of 0 -->
-				{#each results as { score, obj: { id, name, quantity, image_url, all_ids } }}
-					<div class="card w-60 border border-opacity-50">
-						<div class="card-body">
-							<div class="card-actions">
-								<button
-									class="btn btn-primary"
-									on:click={() => {
-										cart.add(id);
-										// Necessary to trigger Svelte's reactivity
-										cart = cart;
-									}}>Add to cart</button
+			<div class="m-4 w-1/2">
+				<h2 class="mb-4 text-center font-display-serif text-3xl">Cart</h2>
+				<form on:submit|preventDefault={checkout}>
+					<input
+						class="input input-bordered mb-2"
+						type="number"
+						min="1"
+						required
+						placeholder="Team number"
+						bind:value={teamNumber}
+					/>
+					<button type="submit" class="btn btn-primary">Finish Checkout</button>
+					<div class="my-2 flex flex-col gap-2">
+						{#each cart as part}
+							<div class="join">
+								<p class="join-item mr-4 place-self-center">Part {part.id}:</p>
+								<input
+									type="number"
+									min="1"
+									class="input join-item input-bordered w-24"
+									required
+									bind:value={part.quantity}
+								/>
+								<button class="btn btn-secondary join-item" on:click={() => removeFromCart(part.id)}
+									>Remove from cart</button
 								>
 							</div>
-							<p>IDs: {all_ids}</p>
-							<p>Score: {score}</p>
-							<h2 class="card-title">{name}</h2>
-							<div class="badge">Quantity: {quantity}</div>
-						</div>
-						<figure>
-							<img src={image_url} alt={name} />
-						</figure>
+						{/each}
 					</div>
-				{/each}
+				</form>
 			</div>
 		</div>
-		<div class="w-1/2">
-			<h2 class="text-center font-display-serif text-3xl">Cart</h2>
-		</div>
 	</div>
-</div>
+{/if}
---
+++ src/routes/admin/checkout/+server.ts
@@ -0,0 +1,51 @@
+import { error, json } from '@sveltejs/kit';
+
+export async function POST({ request, locals: { supabase } }) {
+	const { teamNumber, cart } = await request.json();
+
+	// Check if team exists
+	const { data: team, error: teamError } = await supabase
+		.from('teams')
+		.select()
+		.eq('team_id', teamNumber);
+
+	if (teamError || !team?.length) {
+		throw error(400, `Could not find team ${teamNumber}`);
+	}
+
+	// Update quantities by adding number of already checked out parts
+	const { data: existingParts, error: existingPartsError } = await supabase
+		.from('teams_parts')
+		.select()
+		.eq('team_id', teamNumber);
+
+	if (existingPartsError) {
+		throw error(500, 'Could not get existing parts');
+	}
+
+	console.log(existingParts);
+
+	const checkoutParts = cart.map((part) => {
+		part.id = String(part.id).padStart(4, '0');
+		const already_checked_out =
+			existingParts?.find((existingPart) => existingPart.part_id === part.id)?.quantity ?? 0;
+		console.log(already_checked_out);
+		return {
+			team_id: teamNumber,
+			part_id: part.id,
+			quantity: part.quantity + already_checked_out
+		};
+	});
+
+	// Upsert all the checked out parts
+	const { data: partsData, error: partsError } = await supabase
+		.from('teams_parts')
+		.upsert(checkoutParts)
+		.select();
+
+	if (partsError) {
+		throw error(500, `Could not upsert ${checkoutParts.length} parts: ${partsError.code}`);
+	}
+
+	return json({ teamNumber, partsData });
+}
