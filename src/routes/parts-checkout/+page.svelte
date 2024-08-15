625c840e82377338140b3e54ff5f1526ebae7bb6
daowilliam310 <daowilliam310@gmail.com>
2024-08-15 08:48:58 -0700

    prettier

--- src/routes/parts-checkout/+page.svelte
+++ src/routes/parts-checkout/+page.svelte
@@ -1,6 +1,11 @@
 <script>
-import PartInfoCard from "$lib/components/PartInfoCard.svelte"
-let part1_data = {name: 'Servo', img: "https://www.electronicwings.com/storage/PlatformSection/TopicContent/236/description/servo.jpg", quantity: 20, id: 1443 }
+	import PartInfoCard from '$lib/components/PartInfoCard.svelte';
+	let part1_data = {
+		name: 'Servo',
+		img: 'https://www.electronicwings.com/storage/PlatformSection/TopicContent/236/description/servo.jpg',
+		quantity: 20,
+		id: 1443
+	};
 </script>
 
-<PartInfoCard part_data = {part1_data}/>
+<PartInfoCard part_data={part1_data} />
