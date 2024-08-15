acb58d5b10400280bcec4dd45a260cb3b8af6882
daowilliam310 <daowilliam310@gmail.com>
2024-08-15 08:41:15 -0700

    finish card

---
+++ src/routes/parts-checkout/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+import PartInfoCard from "$lib/components/PartInfoCard.svelte"
+let part1_data = {name: 'Servo', img: "https://www.electronicwings.com/storage/PlatformSection/TopicContent/236/description/servo.jpg", quantity: 20, id: 1443 }
+</script>
+
+<PartInfoCard part_data = {part1_data}/>
