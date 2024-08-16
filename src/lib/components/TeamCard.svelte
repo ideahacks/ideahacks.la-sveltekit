1ba03abc36c28478c6deaf95756da277756a8915
Alexis Lee <nocturne20@g.ucla.edu>
2024-08-16 08:42:41 -0700

    run prettier

--- src/lib/components/TeamCard.svelte
+++ src/lib/components/TeamCard.svelte
@@ -1,20 +1,20 @@
 <script>
-    export let team_info;
+	export let team_info;
 </script>
 
-<div class="card bg-base-100 w-96 shadow-xl">
-    <div class="card-body">
-      <h2 class="card-title">Team {team_info.number}</h2>
-      <h3 class="text-lg font-bold">Members</h3>
-      <ul>
-        {#each team_info.members as member}
-            <li>{member}</li>
-        {/each}
-      </ul>
-      <h3>Parts</h3>
-      <p>{team_info.parts}</p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
-      </div>
-    </div>
+<div class="card w-96 bg-base-100 shadow-xl">
+	<div class="card-body">
+		<h2 class="card-title">Team {team_info.number}</h2>
+		<h3 class="text-lg font-bold">Members</h3>
+		<ul>
+			{#each team_info.members as member}
+				<li>{member}</li>
+			{/each}
+		</ul>
+		<h3>Parts</h3>
+		<p>{team_info.parts}</p>
+		<div class="card-actions justify-end">
+			<button class="btn btn-primary">Buy Now</button>
+		</div>
+	</div>
 </div>
--- src/routes/teams/+page.svelte
+++ src/routes/teams/+page.svelte
@@ -1,12 +1,11 @@
 <script>
-    import TeamCard from "$lib/components/TeamCard.svelte";
+	import TeamCard from '$lib/components/TeamCard.svelte';
 
-    const team1 = {
-        number: 1,
-        members: ["abby", "betty", "caleb"],
-        parts: [23345, 23588, 47810]
-    }
-
+	const team1 = {
+		number: 1,
+		members: ['abby', 'betty', 'caleb'],
+		parts: [23345, 23588, 47810]
+	};
 </script>
 
-<TeamCard team_info={team1}/>
+<TeamCard team_info={team1} />
