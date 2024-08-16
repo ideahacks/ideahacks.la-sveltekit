8999563988c9d4bebf6e162141a0e1d991510449
KevZ1209 <kevinbowditch11@gmail.com>
2024-08-16 23:36:13 +0800

    create initial team card component

---
+++ src/lib/components/TeamCard.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let team_info;
+</script>
+
+<div class="card w-96 bg-base-100 shadow-xl">
+	<div class="card-body">
+		<h2 class="card-title">Team {team_info.number}</h2>
+		<h3 class="text-lg font-bold">Members</h3>
+		<ul>
+			{#each team_info.members as member}
+				<li>{member}</li>
+			{/each}
+		</ul>
+		<h3>Parts</h3>
+		<p>{team_info.parts}</p>
+	</div>
+</div>
---
+++ src/routes/teams/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import TeamCard from '$lib/components/TeamCard.svelte';
+
+	const team1 = {
+		number: 1,
+		members: ['abby', 'betty', 'caleb'],
+		parts: [23345, 23588, 47810]
+	};
+</script>
+
+<TeamCard team_info={team1} />
