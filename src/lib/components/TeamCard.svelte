c0d97494f8d616e141e04a0034ec391c97c6adb3
Alexis Lee <nocturne20@g.ucla.edu>
2024-08-17 23:05:30 -0700

    implement front-end of teams page

--- src/lib/components/TeamCard.svelte
+++ src/lib/components/TeamCard.svelte
@@ -2,19 +2,21 @@
 	export let team_info;
 </script>
 
-<div class="card w-96 bg-base-100 shadow-xl">
-	<div class="card-body">
-		<h2 class="card-title">Team {team_info.number}</h2>
-		<h3 class="text-lg font-bold">Members</h3>
-		<ul>
+<div class="card max-w-[16rem] bg-base-100 bg-opacity-80 shadow-xl border-4 border-custom-red p-1">
+	<div class="card-body p-1">
+		<h2 class="card-title justify-center font-paytone text-md">Team {team_info.number}</h2>
+        <p class="text-center font-encode">Number of members: {team_info.members.length}</p>
+		<h3 class="text-lg font-bold font-paytone">Members</h3>
+		<ul class="list-disc font-encode pl-4">
 			{#each team_info.members as member}
 				<li>{member}</li>
 			{/each}
 		</ul>
-		<h3>Parts</h3>
-		<p>{team_info.parts}</p>
-		<div class="card-actions justify-end">
-			<button class="btn btn-primary">Buy Now</button>
-		</div>
+		<h3 class="text-md font-bold font-paytone">Parts</h3>
+        <p class="font-encode">
+            {#each team_info.parts as part, index}
+                {part}{#if index < team_info.parts.length - 1},&nbsp;{/if}
+            {/each}
+        </p>
 	</div>
 </div>
--- src/routes/teams/+page.svelte
+++ src/routes/teams/+page.svelte
@@ -1,11 +1,37 @@
 <script>
 	import TeamCard from '$lib/components/TeamCard.svelte';
+    import pixelclouds from '$lib/images/pixelclouds.png';
 
 	const team1 = {
 		number: 1,
 		members: ['abby', 'betty', 'caleb'],
-		parts: [23345, 23588, 47810]
+		parts: [23345, 23588, 47810],
 	};
 </script>
 
-<TeamCard team_info={team1} />
+<h1 class="mt-12 mb-6 font-paytone text-5xl font-bold text-white text-center">Teams</h1>
+
+<form class="flex justify-center">
+    <input
+        type="text"
+        name="search"
+        placeholder="Search team/member"
+        class="input input-ghost w-full max-w-xs text-lg text-white placeholder-white caret-white focus:bg-opacity-30 focus:text-white focus:outline-none"
+    />
+</form>
+
+<div class="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-6 pb-24">
+    <TeamCard team_info={team1} />
+    <TeamCard team_info={team1} />
+    <TeamCard team_info={team1} />
+    <TeamCard team_info={team1} />
+    <TeamCard team_info={team1} />
+    <TeamCard team_info={team1} />
+    <TeamCard team_info={team1} />
+
+</div>
+
+<div
+	class="fixed bottom-0 z-0 h-24 w-full bg-contain"
+	style="background-image: url({pixelclouds})"
+/>
