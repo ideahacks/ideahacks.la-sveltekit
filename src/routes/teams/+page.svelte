4cce580a8216955b03a0d13d8b245b8c9be91be1
Alexis Lee <nocturne20@g.ucla.edu>
2024-08-22 23:49:19 -0700

    partly implement search feature with fuzzysort, encountered errors

--- src/routes/teams/+page.svelte
+++ src/routes/teams/+page.svelte
@@ -1,27 +1,48 @@
-<script>
+<script lang="ts">
 	import TeamCard from '$lib/components/TeamCard.svelte';
 	import pixelclouds from '$lib/images/pixelclouds.png';
+	import fuzzysort from 'fuzzysort';
 
-	const team1 = {
+	//const fuzzysort = require('fuzzysort');
+
+	interface Team {
+		number: number;
+		members: string[];
+		parts: number[];
+	}
+
+	const team1: Team = {
 		number: 1,
 		members: ['abby', 'betty', 'caleb'],
 		parts: [23345, 23588, 47810]
 	};
-	const team2 = {
+	const team2: Team = {
 		number: 2,
 		members: ['david', 'elizabeth', 'francis', 'gabriel'],
 		parts: [12345, 23456, 34567, 456789, 567890, 13594]
 	};
-	const team3 = {
+	const team3: Team = {
 		number: 3,
 		members: ['just me lol as a RIDICULOUSLY long name'],
-		parts: ['11111']
+		parts: [11111]
 	};
-	const team4 = {
+	const team4: Team = {
 		number: 4,
 		members: ['someonewithanextremelylongnameidkwhatelsetowritehere', 'short name'],
-		parts: ['12345']
+		parts: [12345]
 	};
+
+	let search = ''; // search content
+
+	const targets: Team[] = [team1, team2, team3, team4];
+
+	$: results = fuzzysort.go(search, targets, {
+		keys: ['number'],
+		all: true
+	});
+
+	//console.log(results.length);
+	console.log('hi');
 </script>
 
 <h1 class="mb-6 mt-12 text-center font-paytone text-5xl font-bold text-white">Teams</h1>
@@ -30,21 +51,20 @@
 	<input
 		type="text"
 		name="search"
+		bind:value={search}
 		placeholder="Search team/member"
 		class="input input-ghost w-full max-w-xs text-lg text-white placeholder-white caret-white focus:bg-opacity-30 focus:text-white focus:outline-none"
 	/>
 </form>
 
-<p class="mt-4 text-center font-encode text-sm text-white">Showing 100 out of 100 teams</p>
+<p class="mt-4 text-center font-encode text-sm text-white">
+	Showing 4 out of {targets.length} teams
+</p>
 
 <div class="flex flex-wrap justify-center gap-4 p-6 pb-24">
-	<TeamCard team_info={team1} />
-	<TeamCard team_info={team2} />
-	<TeamCard team_info={team3} />
-	<TeamCard team_info={team4} />
-	<TeamCard team_info={team1} />
-	<TeamCard team_info={team1} />
-	<TeamCard team_info={team1} />
+	{#each results as team, index}
+		<TeamCard team_info={team} />
+	{/each}
 </div>
 
 <div
