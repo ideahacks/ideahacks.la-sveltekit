097c3a968acafd16d7761f7a79fad5a74bc604e9
daowilliam310 <daowilliam310@gmail.com>
2024-09-02 01:04:22 -0700

    Application Review and Parts Checkout now dynamically display an arbitrary number of applications and parts based on some array. Using carousel to display only a few parts/apps at once

--- src/lib/components/PartInfoCard.svelte
+++ src/lib/components/PartInfoCard.svelte
@@ -11,17 +11,18 @@
 		<p>Quantity: {part_data.quantity}</p>
 		<p>ID: {part_data.id}</p>
 		<div class="card-actions justify-end">
-			<button class="btn btn-primary">Add to Cart</button>
+			<button class="btn btn-error">Add to Cart</button>
 		</div>
 	</div>
 </div>
 
 <style>
 	.card {
-		flex: 1 1 calc(50% - 16px);
-		/* Adjust the percentage to control the number of cards per row */
+		flex: 1 8;
+		min-width: 160px;
+		
 		box-sizing: border-box;
-		width: 300px;
+		max-width: 250px;
 		background-color: #f0f0f0;
 		border-radius: 10px;
 		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
@@ -34,6 +35,11 @@
 	.card-body {
 		padding: 20px;
 	}
-
-	
+	.card-body h2,
+	p {
+		font-family: 'Paytone One', sans-serif;
+	}
+	.btn {
+		flex: 1 1 calc(50% - 16px);
+	}
 </style>
--- src/routes/parts-checkout/+page.svelte
+++ src/routes/parts-checkout/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import PartInfoCard from '$lib/components/PartInfoCard.svelte';
+
 	let part1_data = {
 		name: 'Servo',
 		img: 'https://www.electronicwings.com/storage/PlatformSection/TopicContent/236/description/servo.jpg',
@@ -12,20 +13,72 @@
 		quantity: 5,
 		id: 1550
 	};
+	let parts = [
+		{
+			name: 'Arduino Uno',
+			img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Arduino_Uno_-_R3.jpg',
+			quantity: 5,
+			id: 1550
+		},
+		{
+			name: 'Servo',
+			img: 'https://www.electronicwings.com/storage/PlatformSection/TopicContent/236/description/servo.jpg',
+			quantity: 20,
+			id: 1443
+		},
+		{
+			name: 'Servo',
+			img: 'https://www.electronicwings.com/storage/PlatformSection/TopicContent/236/description/servo.jpg',
+			quantity: 20,
+			id: 1443
+		},
+		{
+			name: 'Servo',
+			img: 'https://www.electronicwings.com/storage/PlatformSection/TopicContent/236/description/servo.jpg',
+			quantity: 20,
+			id: 1443
+		},
+		{
+			name: 'Servo',
+			img: 'https://www.electronicwings.com/storage/PlatformSection/TopicContent/236/description/servo.jpg',
+			quantity: 20,
+			id: 1443
+		}
+	];
+	let grouped_parts = [];
+	for (let i = 0; i < parts.length; i += 4) {
+		grouped_parts.push(parts.slice(i, i + 4));
+	}
 </script>
 
 <div class="title">Parts Checkout</div>
 <div class="layout">
-	<label class="search-label" for="search-input">Search</label>
-	<div class="search-bar-container">
-		<input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-	</div>
+	<div class="left-side">
+		<div class="search-bar-container">
+			<label class="search-label" for="search-input">Search</label>
+			<!-- <div class="search-bar-container">-->
+			<div class="change-bar">
+				<input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+			</div>
+		</div>
+		<!--</div>-->
 
-	<div class="cards-container">
-		<PartInfoCard part_data={part1_data} />
-		<PartInfoCard part_data={part2_data} />
-		<PartInfoCard part_data={part2_data} />
-		<PartInfoCard part_data={part2_data} />
+		<div class="carousel w-full">
+			{#each grouped_parts as item, index}
+				<div id={'item' + (index + 1)} class="carousel-item w-full">
+					<div class="cards-container">
+						{#each item as card}
+							<PartInfoCard part_data={card} />
+						{/each}
+					</div>
+				</div>
+			{/each}
+		</div>
+		<div class="flex w-full justify-center gap-2 py-2">
+			{#each grouped_parts as item, index}
+				<a href={'#item' + (index + 1)} class="btn btn-xs">{index + 1}</a>
+			{/each}
+		</div>
 	</div>
 	<div class="cart-container">
 		<label class="search-label" for="cart-input">Cart</label>
@@ -43,27 +96,52 @@
 <style>
 	.title {
 		text-align: center;
-		font-size: 2em;
+		font-size: 5em;
 		margin-bottom: 16px;
+		font-family: 'Paytone One', sans-serif;
+		color: #ffffff;
+	}
+	.left-side {
+		display: flex;
+		flex-direction: column;
+		place-self: center stretch;
+	}
+	.search-label {
+		font-family: 'Paytone One', sans-serif;
+		color: #ffffff;
+		font-size: 3em;
+	}
+
+	.change-bar {
+		align-items: center;
+		align-self: center;
+		display: flex;
+		margin-bottom: 10px;
 	}
 	.layout {
 		display: grid;
-		grid-template-columns: 1fr 3fr;
-		gap: 16px;
+		grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 	}
 	.search-bar-container {
-		grid-column: 1/-1;
 		display: flex;
-		margin-left: 36px;
-		grid-column: 1/-1;
-		margin-bottom: 16px;
-		text-align: center;
+		flex-direction: column;
+		align-items: center; 
+		margin-top: 16px;
 	}
 	.cards-container {
 		display: flex;
-		margin-left: 20px;
+		align-items: stretch;
+
+		margin-top: 5px;
 		flex-wrap: wrap;
-		gap: 16px; /* Adjust the gap between cards */
+		gap: 8px; 
+		width: 100%;
+	}
+	.carousel {
+		justify-self: center;
+	}
+	.carousel-item {
+		justify-self: center;
 	}
 	.search-label {
 		font-weight: bold;
@@ -72,22 +150,18 @@
 		display: block;
 		text-align: center;
 	}
-	
-	
-	.input {
-		align-self: right;
-	}
+
 	.cart-container {
 		display: flex;
 		flex-direction: column;
-		align-items: center; /* Center items horizontally */
+		align-items: center; 
 		margin-top: 16px;
 	}
 	.cart-input-group {
 		display: flex;
-		align-items: center; /* Align input and button vertically */
+		align-items: center; 
 	}
 	.cart-input {
-		margin-right: 8px; /* Space between input and button */
+		margin-right: 8px; 
 	}
 </style>
