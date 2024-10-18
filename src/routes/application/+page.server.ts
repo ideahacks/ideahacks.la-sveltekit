6413d753541471ab1ce642e5c6ee1576841aea53
Zach Fischer <zfischer42@gmail.com>
2024-10-18 12:25:32 -0700

    Add application page implementing Superform

---
+++ src/routes/application/+page.server.ts
@@ -0,0 +1,16 @@
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { z } from 'zod';
+
+// Define outside the load function so the adapter can be cached
+const schema = z.object({
+  name: z.string().default('Hello world!'),
+  email: z.string().email()
+});
+
+export const load = (async () => {
+  const form = await superValidate(zod(schema));
+
+  // Always return { form } in load functions
+  return { form };
+});
---
+++ src/routes/application/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { superForm } from 'sveltekit-superforms';
+	import SuperDebug from 'sveltekit-superforms';
+
+	export let data;
+
+	// Client API:
+	const { form } = superForm(data.form);
+</script>
+
+<SuperDebug data={$form} />
+
+<form method="POST" class="mx-auto w-full max-w-screen-lg">
+	<!-- First Row -->
+	<!-- Create a component -->
+	<div class="mx-auto flex flex-col items-center justify-center gap-3 sm:flex-row">
+		<!-- Name -->
+		<label class="form-control w-full max-w-xs">
+			<div class="label">
+				<span class="label-text">What is your name?</span>
+			</div>
+			<input
+				name="name"
+				bind:value={$form.name}
+				type="text"
+				placeholder="Type here"
+				class="input input-bordered w-full max-w-xs"
+			/>
+		</label>
+
+		<!-- Email -->
+		<label class="form-control w-full max-w-xs">
+			<div class="label">
+				<span class="label-text">Email</span>
+			</div>
+			<input
+				name="email"
+				bind:value={$form.email}
+				type="email"
+				placeholder="Type here"
+				class="input input-bordered w-full max-w-xs"
+			/>
+		</label>
+	</div>
+
+	<div><button>Submit</button></div>
+</form>
