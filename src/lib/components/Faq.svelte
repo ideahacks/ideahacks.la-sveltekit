308a6354fa4f002980c35ab5d5ce8244a9975116
Kevin Zhang <43306846+KevZ1209@users.noreply.github.com>
2024-12-05 11:41:12 -0800

    Larry faq (#60)
    * initial accordion setup
    
    * Finished first draft of FAQs w/accordian. Will probably try to figure out how to just close a section later.
    
    ---------
    
    Co-authored-by: simonmeatball <62194612+simonmeatball@users.noreply.github.com>

---
+++ src/lib/components/Faq.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let question = 'FAQ';
+	export let answer = 'Answers to common questions about IDEA Hacks';
+</script>
+
+<div class="collapse collapse-arrow mb-4 bg-base-200 bg-opacity-20">
+	<input type="radio" name="my-accordion-2" />
+	<div class="collapse-title font-paytone text-xl font-medium">{question}</div>
+	<div class="collapse-content font-mono">
+		<p>
+			{answer}
+		</p>
+	</div>
+</div>
