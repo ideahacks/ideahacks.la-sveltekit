f4d03dca2ae1ab4488bc083b0563ac6241988a42
Zach Fischer <zfischer42@gmail.com>
2024-12-05 10:46:56 -0800

    Make the success page pretty

--- src/routes/application/+page.server.ts
+++ src/routes/application/+page.server.ts
@@ -54,7 +54,7 @@ export const actions = {
 				delete new_form.school_if_other;
 			}
 
-			const { error } = await supabase.from('applications_2025').insert(new_form).select();
+			const { error } = await supabase.from('applications_2025').upsert(new_form, {onConflict: 'email'}).select();
 
 			if (!error) {
 				console.log('Application submitted successfully!');
