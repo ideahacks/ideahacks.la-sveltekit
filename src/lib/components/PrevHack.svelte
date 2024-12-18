aa6fd28fb2fbc76d097f524c97703e0296822b73
Alexis Lee <nocturne20@g.ucla.edu>
2024-12-18 13:39:18 -0800

    add sponsor logos

---
+++ src/lib/components/PrevHack.svelte
@@ -0,0 +1,12 @@
+<div class="bg-custom-white max-w-[48rem] rounded-xl bg-base-100 bg-opacity-25 p-1 shadow-xl">
+	<div class="mb-10 ml-10 mr-10 text-center">
+		<h1
+			class="hyphenate mb-16 mt-16 justify-center text-center font-encode text-4xl font-bold text-white"
+		>
+			Prev Hack Name
+		</h1>
+		<p class="text-md text-center font-encode text-white">
+			This was a previous hackathon in 202_ centered around…
+		</p>
+	</div>
+</div>
