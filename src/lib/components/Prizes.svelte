39ce4468ef3f0ceb7e7fe48014fd2e6b5059e4e4
Kevin Zhang <43306846+KevZ1209@users.noreply.github.com>
2024-12-19 13:23:24 -0800

    Larry prizes (#66)
    * Add Schedule Timeline (#57)
    
    * finish initial timeline
    
    * run prettier
    
    * Schedule component + Timeline component + broken font??
    
    * schedule component + timeline component
    
    * modify breakpoint behavior
    
    ---------
    
    Co-authored-by: simonmeatball <62194612+simonmeatball@users.noreply.github.com>
    
    * FAQ Section Component (#58)
    
    * initial accordion setup
    
    * Finished first draft of FAQs w/accordian. Will probably try to figure out how to just close a section later.
    
    ---------
    
    Co-authored-by: simonmeatball <62194612+simonmeatball@users.noreply.github.com>
    
    * Added first draft of prizes page with columns
    
    * Added some bouncing to the prizes and some better reactivity (still pretty bad for mobile but better for medium screens)
    
    * Added track prizes in Prizes.svelte component + example images/prizes
    
    * Changed track prizes to be centered vertically too
    
    ---------
    
    Co-authored-by: simonmeatball <62194612+simonmeatball@users.noreply.github.com>

---
+++ src/lib/components/Prizes.svelte
@@ -0,0 +1,93 @@
+<script>
+	import pillar1 from '$lib/images/PILLAR_1.png';
+	import pillar2 from '$lib/images/PILLAR_2.png';
+	import pillar3 from '$lib/images/PILLAR_3.png';
+	import topPrize from '$lib/images/tesla_model_s.png';
+	import pixelclouds from '$lib/images/pixelclouds.png';
+	import superHumanPrize from '$lib/images/Super_Mario.png';
+	import nostalgicPrize from '$lib/images/Old_Radio.png';
+	import insanePrize from '$lib/images/Frakenstein_Statue.png';
+</script>
+
+<div class="relative overflow-hidden rounded-xl bg-white bg-opacity-30 p-5">
+	<!-- Pixel Clouds Overlay -->
+	<div
+		class="pointer-events-none absolute bottom-0 left-0 z-10 h-16 w-full bg-contain bg-center bg-repeat-x sm:h-24 md:h-36 lg:h-48"
+		style="background-image: url({pixelclouds});"
+	/>
+
+	<h1 class="pt-4 text-center font-paytone text-4xl font-bold text-white">TOP PRIZES</h1>
+
+	<!-- Prizes and Pillars -->
+	<div class="z-0 flex items-center justify-center gap-16 overflow-hidden">
+		<div class="relative top-24 w-1/6">
+			<p class="hover pb-4 text-center font-paytone text-base text-white md:text-lg lg:text-2xl">
+				Second Prize
+			</p>
+			<img src={topPrize} alt="Top Prize" class="hover" />
+			<p class="hover pb-4 pt-2 text-center font-encode text-xs text-white md:text-base lg:text-xl">
+				Tesla Model S
+			</p>
+			<img src={pillar1} alt="Pillar 1" />
+		</div>
+
+		<div class="relative top-8 w-1/6">
+			<p class="hover pb-4 text-center font-paytone text-base text-white md:text-lg lg:text-2xl">
+				First Prize
+			</p>
+			<img src={topPrize} alt="Top Prize" class="hover" />
+			<p class="hover pb-4 pt-2 text-center font-encode text-xs text-white md:text-base lg:text-xl">
+				Tesla Model S
+			</p>
+			<img src={pillar2} alt="Pillar 2" />
+		</div>
+
+		<div class="relative top-40 w-1/6">
+			<p class="hover pb-4 text-center font-paytone text-base text-white md:text-lg lg:text-2xl">
+				Third Prize
+			</p>
+			<img src={topPrize} alt="Top Prize" class="hover" />
+			<p class="hover pb-4 pt-2 text-center font-encode text-xs text-white md:text-base lg:text-xl">
+				Tesla Model S
+			</p>
+			<img src={pillar3} alt="Pillar 3" />
+		</div>
+	</div>
+</div>
+
+<div class="relative mt-5 overflow-hidden rounded-xl bg-white bg-opacity-30 p-5">
+	<h1 class="pt-4 text-center font-paytone text-4xl text-white">TRACK PRIZES</h1>
+	<div class="grid grid-cols-1 place-items-center gap-4 px-4 md:grid-cols-2 lg:grid-cols-3">
+		<div class="flex flex-col items-center justify-center">
+			<h2 class="pb-2 pt-4 text-center font-paytone text-2xl text-white">Super Human</h2>
+			<img src={superHumanPrize} alt="Super Human Prize" class="mx-auto w-32" />
+			<p class="pt-2 text-center font-encode text-xl text-white">Super Mario Action Figure!</p>
+		</div>
+		<div class="items-center justify-center">
+			<h2 class="pb-2 pt-4 text-center font-paytone text-2xl text-white">Super Nostalgic</h2>
+			<img src={nostalgicPrize} alt="Nostalgic Prize" class="mx-auto w-80" />
+			<p class="pt-2 text-center font-encode text-xl text-white">Your Grandma's Old Radio</p>
+		</div>
+		<div class="items-center justify-center md:col-span-2 lg:col-span-1">
+			<h2 class="pb-2 pt-4 text-center font-paytone text-2xl text-white">Super (in)sane</h2>
+			<img src={insanePrize} alt="Insane Prize" class="mx-auto w-56" />
+			<p class="pt-2 text-center font-encode text-xl text-white">$200 Frankenstein Statue</p>
+		</div>
+	</div>
+</div>
+
+<style>
+	@keyframes hover {
+		0%,
+		100% {
+			transform: translateY(8px);
+		}
+		50% {
+			transform: translateY(-2px); /* Adjust the height for the hover effect */
+		}
+	}
+
+	.hover {
+		animation: hover 3s ease-in-out infinite; /* Change duration as needed */
+	}
+</style>
---
+++ src/routes/schedule/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Schedule from '$lib/components/Schedule.svelte';
+</script>
+
+<div class="flex min-h-screen flex-col p-4">
+	<Schedule />
+</div>
