285e069ca22ffc606b9e7c818553cb87fba575ae
simonmeatball <62194612+simonmeatball@users.noreply.github.com>
2025-04-03 16:10:35 -0700

    add new homepage

--- src/lib/components/BigFaq.svelte
+++ src/lib/components/BigFaq.svelte
@@ -65,101 +65,87 @@
 		text="Miscellaneous"
 	/>
 
-	{#if section === 'general'}
-		<div class="fade-in">
-			<div class="my-4 font-encode text-xl font-bold text-white xl:text-2xl">General</div>
+	<div class="mt-4">
+		{#if section === 'general'}
+			<div class="fade-in">
+				<Faq
+					question="Will I just be working the whole weekend?"
+					answer="Hacking for 36 hours shouldn’t come at the expense of your mental and physical health. To demonstrate how to stay healthy while working hard, we have many fun events planned throughout the hackathon including guided breaks and miscellaneous mini-games distributed throughout the event."
+				/>
 
-			<Faq
-				question="Will I just be working the whole weekend?"
-				answer="Hacking for 36 hours shouldn’t come at the expense of your mental and physical health. To demonstrate how to stay healthy while working hard, we have many fun events planned throughout the hackathon including guided breaks and miscellaneous mini-games distributed throughout the event."
-			/>
+				<Faq
+					question="Who can attend?"
+					answer="All students interested are encouraged to apply regardless of your major or experience level! And although most participants end up being UCLA engineering students, we would love to welcome participants from any major and those outside the Bruin community :)"
+				/>
 
-			<Faq
-				question="Who can attend?"
-				answer="All students interested are encouraged to apply regardless of your major or experience level! And although most participants end up being UCLA engineering students, we would love to welcome participants from any major and those outside the Bruin community :)"
-			/>
-
-			<Faq
-				question="I don't know anything about hardware!"
-				answer="You are definitely not alone! Before the hackathon, we will have a hacking fundamentals workshop to provide you with all the baseline knowledge you need to hit the ground running once hacking begins! During the hackathon, there will be workshops on various technologies and techniques such as wireless communication and CAD that will be open to everyone. There will also be mentors and other hardware experts ready to assist those who are having trouble bringing their hacks to life."
-			/>
-		</div>
-	{:else if section === 'accommodations'}
-		<div class="fade-in">
-			<div class="mb-4 mt-4 font-encode text-xl font-bold text-white xl:text-2xl">
-				Accommodations
+				<Faq
+					question="I don't know anything about hardware!"
+					answer="You are definitely not alone! Before the hackathon, we will have a hacking fundamentals workshop to provide you with all the baseline knowledge you need to hit the ground running once hacking begins! During the hackathon, there will be workshops on various technologies and techniques such as wireless communication and CAD that will be open to everyone. There will also be mentors and other hardware experts ready to assist those who are having trouble bringing their hacks to life."
+				/>
 			</div>
+		{:else if section === 'accommodations'}
+			<div class="fade-in">
+				<Faq
+					question="How much does it cost?"
+					answer="This event is completely free of charge! We supply everything you need all the parts and food you need for the entire weekend."
+				/>
 
-			<Faq
-				question="How much does it cost?"
-				answer="This event is completely free of charge! We supply everything you need all the parts and food you need for the entire weekend."
-			/>
+				<Faq
+					question="What should I bring?"
+					answer="At the very least, you need a valid ID to check in for the hackathon. For the most up to date list of items to bring, please refer to our pre-event guide."
+				/>
 
-			<Faq
-				question="What should I bring?"
-				answer="At the very least, you need a valid ID to check in for the hackathon. For the most up to date list of items to bring, please refer to our pre-event guide."
-			/>
+				<Faq
+					question="Will food be provided?"
+					answer="We will supply 4 meals, all free of charge! This includes breakfast, lunch and dinner on Saturday as well as breakfast on Sunday. We also have several snacks thanks to our generous sponsors."
+				/>
 
-			<Faq
-				question="Will food be provided?"
-				answer="We will supply 4 meals, all free of charge! This includes breakfast, lunch and dinner on Saturday as well as breakfast on Sunday. We also have several snacks thanks to our generous sponsors."
-			/>
-
-			<Faq
-				question="How about travel costs?"
-				answer="Unfortunately, we cannot cover travel costs. Since most of our participants come from UCLA, we encourage most participants to walk (or bike/scooter) to the venue if possible. For those coming from outside of campus, we encourage you to use public transportation (LA Metro, Santa Manta Blue Bus, Culver City Bus) to minimize your impact on the environment. If you need to use a rental vehicle, we recommend getting an electric car and charging it at one of UCLA’s many EV charging stations."
-			/>
-		</div>
-	{:else if section === 'applications'}
-		<div class="fade-in">
-			<div class="mb-4 mt-4 font-encode text-xl font-bold text-white xl:text-2xl">Applications</div>
-
-			<Faq
-				question="How do I apply?"
-				answer="Hackers can apply on this website! Applications will remain open until late December. Applications are reviewed and accepted on a first come first serve basis, so apply early! Make sure to apply by December 20, 2024 to ensure you get your preferred shirt size, make sure we can order any preferred parts you list on your application, and be entered into a special raffle as a thanks for applying early!"
-			/>
-
-			<Faq
-				question="What if I don't have a team?"
-				answer="We require everyone to be on teams of around 4-5 hackers. Not only will this make it more enjoyable, but more equipment will be available for your use. Before hacking begins, we will hold a team formation social so that everyone can have the opportunity to meet new people and form a team!"
-			/>
-		</div>
-	{:else if section === 'sustainability'}
-		<div class="fade-in">
-			<div class="mb-4 mt-4 font-encode text-xl font-bold text-white xl:text-2xl">
-				Sustainability
+				<Faq
+					question="How about travel costs?"
+					answer="Unfortunately, we cannot cover travel costs. Since most of our participants come from UCLA, we encourage most participants to walk (or bike/scooter) to the venue if possible. For those coming from outside of campus, we encourage you to use public transportation (LA Metro, Santa Manta Blue Bus, Culver City Bus) to minimize your impact on the environment. If you need to use a rental vehicle, we recommend getting an electric car and charging it at one of UCLA’s many EV charging stations."
+				/>
 			</div>
+		{:else if section === 'applications'}
+			<div class="fade-in">
+				<Faq
+					question="How do I apply?"
+					answer="Hackers can apply on this website! Applications will remain open until late December. Applications are reviewed and accepted on a first come first serve basis, so apply early! Make sure to apply by December 20, 2024 to ensure you get your preferred shirt size, make sure we can order any preferred parts you list on your application, and be entered into a special raffle as a thanks for applying early!"
+				/>
 
-			<Faq
-				question="What are you doing to protect the environment?"
-				answer="IDEA Hacks is committed to minimizing our event’s impact on the environment. As the largest hardware-based hackathon on the West Coast, we can set the standard for how large events can operate as sustainably as possible. Moreover, we aim to utilize the reach of the event to educate our 200+ expected participants on how they can establish environmentally-friendly habits and equip them with (free!) supplies to live more sustainably in their day-to-day lives."
-			/>
-
-			<Faq
-				question="What can I do to help?"
-				answer="To help us achieve our goal of making IDEA Hacks 2024 as sustainable as possible, we have a few simple requests from our hackers. Please bring your own water bottles, as well as notebooks, pens, and pencils if you plan on using them. Please pay attention and listen to our waste sorting educational material. Unplug your computer chargers when you’re not using them to minimize “vampire energy”. And, if you want to help us reduce the food wasted by the end of the event, make sure you come hungry and eat enough :)"
-			/>
-		</div>
-	{:else}
-		<div class="fade-in">
-			<div class="mb-4 mt-4 font-encode text-xl font-bold text-white xl:text-2xl">
-				Miscellaneous
+				<Faq
+					question="What if I don't have a team?"
+					answer="We require everyone to be on teams of around 4-5 hackers. Not only will this make it more enjoyable, but more equipment will be available for your use. Before hacking begins, we will hold a team formation social so that everyone can have the opportunity to meet new people and form a team!"
+				/>
 			</div>
+		{:else if section === 'sustainability'}
+			<div class="fade-in">
+				<Faq
+					question="What are you doing to protect the environment?"
+					answer="IDEA Hacks is committed to minimizing our event’s impact on the environment. As the largest hardware-based hackathon on the West Coast, we can set the standard for how large events can operate as sustainably as possible. Moreover, we aim to utilize the reach of the event to educate our 200+ expected participants on how they can establish environmentally-friendly habits and equip them with (free!) supplies to live more sustainably in their day-to-day lives."
+				/>
 
-			<Faq
-				question="What tools and hardware will be provided for my team?"
-				answer="If you would like to see a specific part or device at the hackathon, you can specify so on your application and we'll do our best to accommodate! You can also view a full list of available parts on our Parts page closer to the start of the hackathon. We will also provide soldering stations, 3D printers, and laser cutters for you to use on your projects."
-			/>
+				<Faq
+					question="What can I do to help?"
+					answer="To help us achieve our goal of making IDEA Hacks 2024 as sustainable as possible, we have a few simple requests from our hackers. Please bring your own water bottles, as well as notebooks, pens, and pencils if you plan on using them. Please pay attention and listen to our waste sorting educational material. Unplug your computer chargers when you’re not using them to minimize “vampire energy”. And, if you want to help us reduce the food wasted by the end of the event, make sure you come hungry and eat enough :)"
+				/>
+			</div>
+		{:else}
+			<div class="fade-in">
+				<Faq
+					question="What tools and hardware will be provided for my team?"
+					answer="If you would like to see a specific part or device at the hackathon, you can specify so on your application and we'll do our best to accommodate! You can also view a full list of available parts on our Parts page closer to the start of the hackathon. We will also provide soldering stations, 3D printers, and laser cutters for you to use on your projects."
+				/>
 
-			<Faq
-				question="How can I check out parts during the hackathon?"
-				answer="Our Parts page will let you check which parts are available, and from there you'll be able to go to our parts table and have a volunteer check the part out for you. Some of our parts are consumable, but many need to be scanned so we can keep track of them. Due to changing part availability with recent part shortages, we will update this page as we get closer to the event date with any changes to our inventory."
-			/>
+				<Faq
+					question="How can I check out parts during the hackathon?"
+					answer="Our Parts page will let you check which parts are available, and from there you'll be able to go to our parts table and have a volunteer check the part out for you. Some of our parts are consumable, but many need to be scanned so we can keep track of them. Due to changing part availability with recent part shortages, we will update this page as we get closer to the event date with any changes to our inventory."
+				/>
 
-			<Faq
-				question="How do I contact IDEA Hacks?"
-				answer="Email us at evp@ieeebruins.com! We would love to hear from you!"
-			/>
-		</div>
-	{/if}
+				<Faq
+					question="How do I contact IDEA Hacks?"
+					answer="Email us at evp@ieeebruins.com! We would love to hear from you!"
+				/>
+			</div>
+		{/if}
+	</div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -40,7 +40,7 @@
 
 <!-- NAVBAR -->
 <img src={logo} alt="logo" class="fixed right-36 top-36 z-0 w-96 opacity-15" />
-<div class="navbar z-50 h-6 w-full text-white">
+<div class="navbar sticky top-0 z-50 h-6 w-full text-white">
 	<div class="navbar-start">
 		<ul class="font-paytone">
 			<a href="/" class="btn btn-ghost text-xl"
@@ -67,8 +67,8 @@
 				</svg>
 			</div>
 			<ul
-				tabindex="0"
-				class="menu dropdown-content menu-sm z-50 mt-3 w-52 rounded-box bg-[#2775af] p-2 font-encode font-bold shadow"
+				tabindex="-1"
+				class="menu dropdown-content menu-sm z-50 mt-3 w-52 rounded-box p-2 font-encode font-bold shadow"
 			>
 				<li>
 					<a href="/sponsor-us">Sponsor Us!</a>
@@ -116,71 +116,12 @@
 	</div>
 </div>
 
-<!-- CLOUDS -->
-{#if wave1}
-	<img
-		src={cloud1}
-		transition:fly={{ duration: 50000, x: '150vw', opacity: 1, easing: linear }}
-		alt="cloud"
-		class="fixed -left-96 top-10 z-0 w-96 opacity-25"
-	/>
-	<img
-		src={cloud2}
-		transition:fly={{ duration: 20000, x: '50vw', opacity: 1, easing: linear }}
-		alt="cloud"
-		class="fixed -left-96 top-96 z-0 w-96 opacity-25"
-	/>
-	<img
-		src={cloud2}
-		transition:fly={{ duration: 30000, x: '100vw', opacity: 1, easing: linear }}
-		alt="cloud"
-		class="fixed -left-96 bottom-20 z-0 w-96 opacity-25"
-	/>
-{/if}
-
-{#if wave2}
-	<img
-		src={cloud3}
-		transition:fly={{ duration: 50000, x: '150vw', opacity: 1, easing: linear }}
-		alt="cloud"
-		class="fixed -left-96 top-36 z-0 w-96 opacity-25"
-	/>
-	<img
-		src={cloud4}
-		transition:fly={{ duration: 50000, x: '125vw', opacity: 1, easing: linear }}
-		alt="cloud"
-		class="fixed -left-96 bottom-36 z-0 w-96 opacity-25"
-	/>
-{/if}
-
-{#if wave3}
-	<img
-		src={cloud3}
-		transition:fly={{ duration: 50000, x: '100vw', opacity: 1, easing: linear }}
-		alt="cloud"
-		class="fixed -left-96 bottom-32 z-0 w-96 opacity-25"
-	/>
-	<img
-		src={cloud1}
-		transition:fly={{ duration: 50000, x: '150vw', opacity: 1, easing: linear }}
-		alt="cloud"
-		class="fixed -left-96 top-10 z-0 w-96 opacity-25"
-	/>
-	<img
-		src={cloud4}
-		transition:fly={{ duration: 50000, x: '125vw', opacity: 1, easing: linear }}
-		alt="cloud"
-		class="fixed -left-96 top-96 z-0 w-96 opacity-25"
-	/>
-{/if}
-
 <div class="z-0 flex min-h-screen flex-col">
 	<slot />
 </div>
 
 <style lang="postcss">
 	:global(body) {
-		background: linear-gradient(180deg, #2775af 0%, #83c3e8 100%);
-		background-color: #83c3e8;
+		background-color: black;
 	}
 </style>
