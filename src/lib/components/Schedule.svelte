d3876835c9bc1a08f537696dd9643311a8d106f8
KevZ1209 <kevinbowditch11@gmail.com>
2025-05-02 12:07:15 -0700

    change schedule info

--- src/lib/components/Schedule.svelte
+++ src/lib/components/Schedule.svelte
@@ -7,31 +7,31 @@
 			Start: '4:30 PM',
 			End: '8:00 PM',
 			Event: 'Check In',
-			Location: 'Ackerman Grand Ballroom'
+			Location: 'Covel Grand Horizon'
 		},
 		{
-			Start: '5:00 PM',
-			End: '5:45 PM',
-			Event: 'Tentative: Webdev workshop',
-			Location: 'Ackerman Union 2408'
+			Start: '5:30 PM',
+			End: '7:00 PM',
+			Event: 'Using SOC Boards (Digikey/Infineon)',
+			Location: 'Covel Grand Horizon'
 		},
 		{
 			Start: '6:00 PM',
 			End: '6:45 PM',
 			Event: '3D Printing / CAD workshop',
-			Location: 'Ackerman Union 2412'
+			Location: 'Conference Room 1'
 		},
 		{
 			Start: '7:00 PM',
 			End: '8:00 PM',
 			Event: 'Teambuilding workshop',
-			Location: 'Ackerman Grand Ballroom'
+			Location: 'Covel Grand Horizon'
 		},
 		{
 			Start: '8:30 PM',
 			End: '9:00 PM',
 			Event: 'Opening presentation',
-			Location: 'Ackerman Grand Ballroom'
+			Location: 'Covel Grand Horizon'
 		},
 		{
 			Start: '8:40 PM',
@@ -43,91 +43,109 @@
 			Start: '9:00 PM',
 			End: '',
 			Event: 'Hacking begins',
-			Location: 'Ackerman Grand Ballroom'
+			Location: 'Covel Grand Horizon'
 		},
 		{
 			Start: '9:15 PM',
 			End: '10:00 PM',
-			Event: 'Microcontroller Selection workshop',
-			Location: 'Ackerman Union 2408'
+			Event: 'Microcontroller Selection Workshop',
+			Location: 'Conference Room 1'
 		},
 		{
-			Start: '10:15 PM',
-			End: '11:00 PM',
+			Start: '10:00 PM',
+			End: '10:45 PM',
 			Event: 'Wireless Connectivity workshop',
-			Location: 'Ackerman Union 2412'
+			Location: 'Conference Room 2'
+		},
+		{
+			Start: '11:00 PM',
+			End: '',
+			Event: 'Venue Closes',
+			Location: ''
 		}
 	];
 
 	const saturdaySchedule = [
 		{
+			Start: '7:30 AM',
+			End: '',
+			Event: 'Venue Opens',
+			Location: 'Covel Grand Horizon'
+		},
+		{
 			Start: '8:00 AM',
 			End: '10:00 AM',
 			Event: 'Breakfast',
-			Location: 'Ackerman Grand Ballroom'
+			Location: 'Covel Grand Horizon'
 		},
 		{
 			Start: '10:00 AM',
 			End: '10:30 AM',
 			Event: 'Breakfast meals to go',
-			Location: 'Ackerman Grand Ballroom'
+			Location: 'Covel Grand Horizon'
 		},
 		{
 			Start: '10:00 AM',
 			End: '12:00 PM',
-			Event: 'Digikey & Infineon Workshop',
-			Location: 'Ackerman Grand Ballroom'
+			Event: 'Product Design Workshop',
+			Location: 'Covel Grand Horizon'
 		},
 		{
 			Start: '12:00 PM',
 			End: '1:30 PM',
 			Event: 'Lunch',
-			Location: 'Ackerman Grand Ballroom'
-		},
-		{
-			Start: '1:30 PM',
-			End: '3:00 PM',
-			Event: 'Graduate School Tabling',
-			Location: 'Ackerman Union 2412'
+			Location: 'Covel Grand Horizon'
 		},
 		{
 			Start: '4:00 PM',
 			End: '5:00 PM',
-			Event: 'Product Design Workshop',
-			Location: 'Ackerman Union 2408'
+			Event: 'Digikey Infosession',
+			Location: 'Conference Room 2'
 		},
 		{
 			Start: '6:00 PM',
 			End: '7:30 PM',
 			Event: 'Dinner',
-			Location: 'Ackerman Grand Ballroom'
+			Location: 'Covel Grand Horizon'
 		},
 		{
 			Start: '7:30 PM',
 			End: '8:00 PM',
 			Event: 'Dinner meals to go',
-			Location: 'Ackerman Grand Ballroom'
+			Location: 'Covel Grand Horizon'
 		},
 		{
-			Start: '7:30 PM',
-			End: '10:30 PM',
+			Start: '9:00 PM',
+			End: '12:00 AM',
 			Event: 'Frog Night',
-			Location: 'Downstairs Makerspace'
+			Location: 'Makerspace Floor 1'
+		},
+		{
+			Start: '11:00 PM',
+			End: '',
+			Event: 'Venue Closes',
+			Location: 'Covel Grand Horizon'
 		}
 	];
 
 	const sundaySchedule = [
 		{
+			Start: '7:30 AM',
+			End: '',
+			Event: 'Venue Opens',
+			Location: 'Covel Grand Horizon'
+		},
+		{
 			Start: '8:00 AM',
 			End: '9:30 AM',
 			Event: 'Breakfast',
-			Location: 'Ackerman Grand Ballroom'
+			Location: 'Covel Grand Horizon'
 		},
 		{
 			Start: '9:00 AM',
 			End: '',
 			Event: 'Hacking ends and Submissions due',
-			Location: 'Ackerman Grand Ballroom'
+			Location: 'Covel Grand Horizon'
 		},
 		{
 			Start: '9:00 AM',
@@ -139,13 +157,13 @@
 			Start: '9:15 AM',
 			End: '10:45 AM',
 			Event: 'Round 1 judging',
-			Location: 'Ackerman Union 2408, 2410, 2412'
+			Location: 'Conference Room 1, 2, 3'
 		},
 		{
 			Start: '11:00 AM',
 			End: '12:30 PM',
 			Event: 'Judging Finals',
-			Location: 'Ackerman Grand Ballroom'
+			Location: 'Covel Grand Horizon'
 		},
 		{
 			Start: '12:30 PM',
@@ -157,7 +175,7 @@
 			Start: '12:40 PM',
 			End: '1:00 PM',
 			Event: 'Awards and closing',
-			Location: 'Ackerman Grand Ballroom'
+			Location: 'Covel Grand Horizon'
 		},
 		{
 			Start: '1:00 PM',
